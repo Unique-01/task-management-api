d3fb9932ecd3a20b9f4e749a023d65e5cf7964dc
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2025-06-27 11:57:23 +0100

    Implement cors to allow requests from all origin

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 require("./config/mongoose.js");
 const express = require("express");
+const cors = require("cors");
 
 const userRouter = require("./routes/userRoutes.js");
 const taskRouter = require("./routes/taskRoutes.js");
@@ -10,6 +11,7 @@ const app = express();
 PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cors()); // Allow requests from all origin for developement simplicity
 
 app.get("/", (req, res) => {
     res.status(200).send("API is running");
